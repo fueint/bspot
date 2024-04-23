e634770350b6aa9ae6b7fdc3057235a49a691ad5
Siva <sivakumaran@dialedin.in>
2024-04-23 16:48:29 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buggerspot</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+    <header class="header">
+        <nav class="d-flex container my-3 nav position-relative">
+            <div class="d-flex align-items-center w-50 buggerspot">
+                <div>
+                    <a src="#" class="d-inline-block"><img src="https://blogger.googleusercontent.com/img/a/AVvXsEic7Odm2AmAarbzqyawIXrt0OZSeTvxcVOj84AWYd6nRLCGNqkzrIMVEserac7x4zjFYH9ks1PdYOIoQLyHQaAaRC0su6Apd9kRG_-6E7ziDHbsq3Cbc4AtN2ARApGrXWGqFU5xi7pyUYy_jJ_-DHqEwJWGJPHqMz5NIxusO-1w8xJPRTm1vtHi8drY=s291" class="vertical-align-middle"></a>
+                </div>
+                <div class="">
+                    <p class="my-0 ml-1"><a src="#">Buggerspot</a></p>
+                </div>
+            </div>
+            <ul class="list-stye-none d-flex w-md-50 justify-content-end my-2">
+                <li class="mr-3"><span id="hamburger" class="cursor-pointer"></span></li>
+                <li class="mr-3"><a href="#">Home</a></li>
+                <li class="mr-3"><a href="#">About</a></li>
+                <li class="mr-3"><a href="#">Contact</a></li>
+                <li class="mr-3"><a href="#">Blogs</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section>
+            <div class="container">
+                <!-- <h1>Welcome to Buggerspot</h1>
+                <h2>This is problem solving bugger.</h2> 
+                <p>i am learning web based technologies and i want to share my knowledge with you and stealing knowledge from you. because if something stagnets in a one place it is not a knowledge it is known as swelling</p> -->
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <!-- <p>&copy; 2024 Your Company</p> -->
+    </footer>
+
+</body>
+</html>
+
+<style>
+    /* Common class */
+    .container {
+        padding-left: 12px;
+        padding-right: 12px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    .d-flex {
+        display: flex;
+    }
+    .d-inline-block {
+        display: inline-block;
+    }
+    .align-items-center {
+        align-items: center;
+    }
+    .justify-content-end {
+        justify-content: end;
+    }
+    .vertical-align-middle {
+        vertical-align: middle;
+    }
+    .position-relative {
+        position: relative;
+    }
+    .w-50 {
+        width: 50%;
+    }
+    .list-stye-none {
+        list-style: none;
+    }
+    .cursor-pointer {
+        cursor: pointer;
+    }
+    .ml-0, .mx-0 {
+        margin-left: 0;
+    }
+    .ml-1, .mx-1 {
+        margin-left: 5px;
+    }
+    .ml-2, .mx-2 {
+        margin-left: 15px;
+    }
+    .ml-3, .mx-3 {
+        margin-left: 25px;
+    }
+    .ml-4, .mx-4 {
+        margin-left: 35px;
+    }
+    .ml-5, .mx-5 {
+        margin-left: 45px;
+    }
+    .mr-0, .mx-0 {
+        margin-right: 0;
+    }
+    .mr-1, .mx-1 {
+        margin-right: 5px;
+    }
+    .mr-2, .mx-2 {
+        margin-right: 15px;
+    }
+    .mr-3, .mx-3 {
+        margin-right: 25px;
+    }
+    .mr-4, .mx-4 {
+        margin-right: 35px;
+    }
+    .mr-5, .mx-5 {
+        margin-right: 45px;
+    }
+
+    .mt-0, .my-0 {
+        margin-top: 0;
+    }
+    .mt-1, .my-1 {
+        margin-top: 5px;
+    }
+    .mt-2, .my-2 {
+        margin-top: 15px;
+    }
+    .mt-3, .my-3 {
+        margin-top: 25px;
+    }
+    .mt-4, .my-4 {
+        margin-top: 35px;
+    }
+    .mt-5, .my-5 {
+        margin-top: 45px;
+    }
+    .mb-0, .my-0 {
+        margin-bottom: 0;
+    }
+    .mb-1, .my-1 {
+        margin-bottom: 5px;
+    }
+    .mb-2, .my-2 {
+        margin-bottom: 15px;
+    }
+    .mb-3, .my-3 {
+        margin-bottom: 25px;
+    }
+    .mb-4, .my-4 {
+        margin-bottom: 35px;
+    }
+    .mb-5, .my-5 {
+        margin-bottom: 45px;
+    }
+
+    .pl-0, .px-0 {
+        padding-left: 0;
+    }
+    .pl-1, .px-1 {
+        padding-left: 5px;
+    }
+    .pl-2, .px-2 {
+        padding-left: 15px;
+    }
+    .pl-3, .px-3 {
+        padding-left: 25px;
+    }
+    .pl-4, .px-4 {
+        padding-left: 35px;
+    }
+    .pl-5, .px-5 {
+        padding-left: 45px;
+    }
+    .pr-0, .px-0 {
+        padding-right: 0;
+    }
+    .pr-1, .px-1 {
+        padding-right: 5px;
+    }
+    .pr-2, .px-2 {
+        padding-right: 15px;
+    }
+    .pr-3, .px-3 {
+        padding-right: 25px;
+    }
+    .pr-4, .px-4 {
+        padding-right: 35px;
+    }
+    .pr-5, .px-5 {
+        padding-right: 45px;
+    }
+
+    .pt-0, .py-0 {
+        padding-top: 0;
+    }
+    .pt-1, .py-1 {
+        padding-top: 5px;
+    }
+    .pt-2, .py-2 {
+        padding-top: 15px;
+    }
+    .pt-3, .py-3 {
+        padding-top: 25px;
+    }
+    .pt-4, .py-4 {
+        padding-top: 35px;
+    }
+    .pt-5, .py-5 {
+        padding-top: 45px;
+    }
+    .pb-0, .py-0 {
+        padding-bottom: 0;
+    }
+    .pb-1, .py-1 {
+        padding-bottom: 5px;
+    }
+    .pb-2, .py-2 {
+        padding-bottom: 15px;
+    }
+    .pb-3, .py-3 {
+        padding-bottom: 25px;
+    }
+    .pb-4, .py-4 {
+        padding-bottom: 35px;
+    }
+    .pb-5, .py-5 {
+        padding-bottom: 45px;
+    }
+
+    /* Global Css Updates */
+    body {
+        margin: 0;
+        padding: 0;
+        background: url('Assets/Images/nature.jpg');
+        background-size: cover;
+        font-family: sans-serif;
+        font-size: 16px;
+    }
+    a {
+        text-decoration: none;
+    }
+    main a {
+        color: #00F7FF;
+    }
+    header img {
+        width: 30px;
+        height: 30px;
+    }
+    header nav {
+        box-shadow: 0px 2px 25px rgba(0, 0, 0, 0.2);
+        border-radius: 7px;
+    }
+    header nav a {
+        color: #000;
+        font-weight: 600;
+        cursor: pointer;
+    }
+    .buggerspot {
+        z-index: 9;
+    }
+    section {
+        padding-top: 20px;
+        padding-bottom: 20px;
+    }
+
+    /* Responsive style */
+    @media only screen and (max-width: 575px) {
+        .header .nav {
+            margin-left: 20px;
+            margin-right: 20px;
+        }
+    }
+    @media only screen and (max-width: 767px) {
+        #hamburger:before { 
+            content:'\2630'; 
+        }
+        .header.nav-open .nav #hamburger:before {
+            content: '\2715';
+            font-size: 20px;
+            line-height: 20px;
+        }
+        .header.nav-open .nav {
+            box-shadow: unset;
+            border-radius: unset;
+        }
+        .header .nav {
+            padding: 10px 12px;
+        }
+        .header .nav ul {
+            flex-direction: column;
+            position: absolute;
+            right: 0;
+            top: -15px;
+            text-align: right;
+            width: 100%;
+            padding-inline-start: 0;
+        }
+        .header.nav-open .nav ul {
+            box-shadow: 0px 2px 25px rgba(0, 0, 0, 0.2);
+            border-radius: 7px;
+        }
+        .header.nav-open .nav ul li {
+            display: block;
+        }
+        .header .nav ul li {
+            padding-bottom: 12px;
+            display: none;
+        }
+        .header .nav ul li:first-child {
+            padding-top: 16px;
+            display: block;
+        }
+    }
+    @media only screen and (min-width: 576px) {
+        .container {
+            max-width: 540px;
+        }
+    }
+    @media only screen and (min-width: 768px) {
+        .container {
+            max-width: 730px;
+        }
+        .w-md-50 {
+            width: 50%;
+        }
+        .header .nav ul li:first-child {
+            display: none;
+        }
+    }
+    @media only screen and (min-width: 992px) {
+        .container {
+            max-width: 980px;
+        }
+    }
+    @media only screen and (min-width: 1200px) {
+        .container {
+            max-width: 1140px;
+        }
+    }
+    @media only screen and (min-width: 1400px) {
+        .container {
+            max-width: 1320px;
+        }
+    }
+</style>
+<script type="text/javascript">
+    var hamBurger = document.getElementById('hamburger');
+    var headerNav = document.querySelector('.header');
+    hamBurger.addEventListener('click', function() {
+        headerNav.classList.toggle("nav-open");
+    });
+</script>
